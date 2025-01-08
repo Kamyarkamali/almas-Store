adc37de49268798b203aaeb6746492faf42b925a
kamyarkamali <kamyarkamali021@gmail.com>
2025-01-08 03:50:42 +0330

    create modal produtcs

--- components/element/ModaProducts.tsx
+++ components/element/ModaProducts.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
 import { FC } from "react";
 
 import { Dispatch, SetStateAction } from "react";
+import CloseSvg from "../icon/CloseSvg";
 
 interface ModaProductsProps {
   image: string;
   nameProduct: string;
+  category: string;
+  category1: string;
+  description: string;
   openModal: boolean;
+  id: string;
   setOpenModal: Dispatch<SetStateAction<boolean>>;
 }
 
@@ -13,6 +19,11 @@ const ModaProducts: FC<ModaProductsProps> = ({
   openModal,
   setOpenModal,
   image,
+  nameProduct,
+  category,
+  category1,
+  description,
+  id,
 }) => {
   return (
     <div
@@ -21,16 +32,53 @@ const ModaProducts: FC<ModaProductsProps> = ({
       } fixed top-0 left-0 w-full h-full bg-black/55 backdrop-blur-md z-[200] flex items-center justify-center`}
     >
       {/* باکس مودال */}
-      <div className="bg-white p-6 rounded-md shadow-lg w-11/12 md:w-1/2 lg:w-1/3">
-        <h2 className="text-2xl font-semibold mb-4">عنوان مودال</h2>
-        <p>محتوای مودال اینجا قرار می‌گیرد.</p>
-        <img src={image} alt="/" />
-        <button
+      <div className="bg-white p-6 flex flex-col items-center rounded-md shadow-lg w-11/12 md:w-1/2 lg:w-1/3">
+        {/* بستن مودال */}
+        <div
+          className="flex justify-end w-full cursor-pointer"
           onClick={() => setOpenModal(false)}
-          className="mt-4 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600"
         >
-          بستن
-        </button>
+          <CloseSvg width="17px" height="17px" />
+        </div>
+        <img src={image} alt={description} />
+        {/* دکمه دیدن جزئیات */}
+        {/* جدا کننده */}
+        <div className="w-full border-t-[1px] border-[#000000] mt-2 py-2"></div>
+        <div className="w-full">
+          <Link
+            href={`/product-category/${id}`}
+            className="bg-[#D60444] w-full inline-block text-center text-white py-2 px-4 rounded-md"
+          >
+            دیدن جزئیات
+          </Link>
+        </div>
+        <div className="w-full">
+          <p className="w-[400px] mt-4 text-[14px] font-bold text-right">
+            {nameProduct}
+          </p>
+        </div>
+        {/* دسته بندی های صفحه */}
+        <section className="flex gap-2 mt-2 justify-start w-full *:text-[14px]">
+          <p>دسته بندی:</p>
+          <p className="text-[#777777]">{category}</p>
+          <p className="text-[#777777]">,</p>
+          <p className="text-[#777777]">{category1}</p>
+        </section>
+
+        {/* قسمت اشتراک گذاری */}
+        <section className="flex items-center mt-[1rem] gap-2 w-full justify-start">
+          <p className="text-[14px]">اشتراک گذاری:</p>
+          <img
+            className="w-[20px] cursor-pointer"
+            src="/svgs/telegram.svg"
+            alt="telegram"
+          />
+          <img
+            className="w-[20px] cursor-pointer"
+            src="/svgs/watsApp.svg"
+            alt="whatsapp"
+          />
+        </section>
       </div>
     </div>
   );
