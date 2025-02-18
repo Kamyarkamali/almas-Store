bbf00404afe9b65dfc51b2186dd7a9a6d78c832f
kamyarkamali <kamyarkamali021@gmail.com>
2025-02-18 14:56:29 +0330

    debug form createblog

--- components/adminComponent/FormCreatedBlogs.tsx
+++ components/adminComponent/FormCreatedBlogs.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import Loader from "@/ui/Loader";
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
